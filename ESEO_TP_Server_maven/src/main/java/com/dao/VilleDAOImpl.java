8e2e11e28b3fe219b4d919a3de511593cb346ec3
Ogrys <adelin.a@outlook.fr>
2020-11-10 13:46:27 +0100

    intial commit

---
+++ ESEO_TP_Server_maven/src/main/java/com/dao/VilleDAOImpl.java
@@ -0,0 +1,21 @@
+package com.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.dto.Ville;
+
+@Repository
+public class VilleDAOImpl implements VilleDAO {
+
+	@Override
+	public ArrayList<Ville> findAllVilles() {
+		ArrayList<Ville> listVille = new ArrayList<Ville>();
+		
+		//TODO requete sql
+		return listVille;
+	}
+	
+
+}
